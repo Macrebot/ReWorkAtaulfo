f24a0c67741149f289b2f04de5f804dfd445defd
Macrebot <53368796+Macrebot@users.noreply.github.com>
2022-02-09 12:39:07 +0100

    Create news.php
    Archivo en el que se va a crear toda la sección de noticias.

---
+++ news.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilos.css">
+    <title>IES Ataúlfo Argenta</title>
+</head>
+<body>
+
+    <div id="header">
+
+    </div>
+
+    <div id="nav">
+
+    </div>
+    
+    <div id="body">
+
+    </div>
+
+    <div id="footer">
+
+    </div>
+
+</body>
+</html>
